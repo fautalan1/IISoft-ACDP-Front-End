bb9c3b6abce77bcc771fda6de69e4ce1015f2067
VictorD <flame.el22@gmail.com>
2018-06-21 23:44:03 -0300

    Se implemento la vista de perfiles de un usuario
    Se modifico la vista de perfil para que muestre el perfil de la cuenta, el personal y el laboral.
    Se separo la responsabilidad de la pegada a la API para obtener los usuarios a un service.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import '../App.css';
 import { Route, Switch } from 'react-router-dom'
 import Header from '../components/Header';
-import axios from 'axios';
 import { NotificationContainer, NotificationManager } from 'react-notifications'
 import 'react-notifications/lib/notifications.css'
 
 import Home from './Home';
 import Schedule from './Schedule';
-import NewPublication from './NewPublication'
 import Perfil from './Perfil'
 import Login from './Login'
 import Users from './Users'
+import UserService from '../Services/UserService'
 
 export default class App extends Component {
 
@@ -43,17 +42,19 @@ export default class App extends Component {
     })
   }
 
-  verify = () => {
-    axios.get('http://localhost:8080/user/' + this.state.name)
-    .then(response => {
-      const user = response.data
-      this.setState({ user })
-      this.setState({name: user.userName})
-      this.login()
-    })
-    .catch(err => {
-      this.createNotification()
-    })
+  verify = async () => {
+    const userService = new UserService()
+    
+    userService.getUser(this.state.name).then(response => 
+                                                {
+                                                  const user = response.data
+                                                  userService.SetUser(user)
+                                                  this.setState({ user })
+                                                  this.setState({name: user.userName})
+                                                  this.login()
+                                                }
+                                              )
+                                        .catch(err => { this.createNotification() } )
   }
 
   handleChange = (ev, input)=>{
@@ -84,7 +85,6 @@ export default class App extends Component {
             <Route exact path='/login' component={Login}/>
             <Route exact path='/usuarios' component={Users}/>
             <Route       path='/schedule' component={Schedule}/>
-            <Route       path='/newPublication' component={NewPublication}/>
           </Switch>
       </div>
     );
--- src/containers/Perfil.js
+++ src/containers/Perfil.js
@@ -1,38 +1,121 @@
 import React, { Component } from 'react'
-import axios from 'axios';
+import { Label, Segment, Grid, Accordion, List } from 'semantic-ui-react'
+import UserService from '../Services/UserService'
 
-export default class Home extends Component {
+const styles=   {   rowProfileTittleStyle   : { width       :'50vh',
+                                                textAlign   :'left'},
+                    tittleProfileStyle      : { width       :'80vh' }
+                }
 
-    constructor(props) { 
-        super(props);
+export default class Perfil extends Component {
+    
+    constructor(props) {
+        super(props)    
+        this.userService = new UserService()
         this.state = {
-            user: ""
+            activeInfoAccount: false,
+            activeInfoPersonal: false,
+            activeInfoProfessional: false,
+            user: this.userService.GetUserLogged()
         }
+        console.log(this.state.user)
     }
 
-    setUser = async () => { 
-        axios.get('http://localhost:8080/user/' + this.props.match.params.userName)
-        .then(response => {
-          const user = response.data;
-          this.setState({ user });
-        })
-    }
-
-    componentDidMount = () => {
-        this.setUser()
-    }
-
-    componentDidUpdate=(prevProps, prevState) => {
-        if(prevProps.match.params.userName !== this.props.match.params.userName) { 
+    componentDidUpdate = (prevProps, prevState) => {
+        if (prevProps.match.params.userName !== this.props.match.params.userName) {
             this.setUser()
         }
     }
 
-    render(){
-        return (
-          <div>
-              {this.state.user.userName}           
-          </div>
-        )
+    handleInfoAccount = () => {
+        this.setState({activeInfoAccount: !this.state.activeInfoAccount})
       }
+
+    handleInfoPersonal = () => {
+        this.setState({activeInfoPersonal: !this.state.activeInfoPersonal})
+    }
+
+    handleInfoProfessional = () => {
+        this.setState({activeInfoProfessional: !this.state.activeInfoProfessional})
+    }
+
+    render() {
+
+        return (
+            <Segment basic secondary>
+                <Grid centered >
+                    <Grid.Row>
+                        <Accordion>
+                            <Accordion.Title active={this.state.activeInfoAccount} onClick={this.handleInfoAccount}>
+                                <Label style={styles.tittleProfileStyle} color='black' ribbon>Informacion De La Cuenta</Label>
+                            </Accordion.Title>
+                            <Accordion.Content active={this.state.activeInfoAccount}>
+                                <Label style={styles.rowProfileTittleStyle} color='grey'>
+                                    Nombre de Usuario: 
+                                    <Label.Detail >{this.state.user.userName}</Label.Detail> 
+                                </Label>
+                            </Accordion.Content>
+                  
+                            <Accordion.Title active={this.state.activeInfoPersonal} onClick={this.handleInfoPersonal}>
+                                <Label style={styles.tittleProfileStyle} color='black' ribbon>Informacion Personal</Label>
+                            </Accordion.Title>
+                            <Accordion.Content active={this.state.activeInfoPersonal}>
+                                <Grid.Row>
+                                    <Label style={styles.rowProfileTittleStyle} color='grey'>
+                                        Nombre: 
+                                        <Label.Detail >{this.state.user.name}</Label.Detail> 
+                                    </Label>
+                                </Grid.Row>
+                                <Grid.Row >
+                                    <Label style={styles.rowProfileTittleStyle} color='grey'>
+                                        Apellido: 
+                                        <Label.Detail >{this.state.user.surname}</Label.Detail> 
+                                    </Label>
+                                </Grid.Row>
+                                <Grid.Row >
+                                    <Label style={styles.rowProfileTittleStyle} color='grey'>
+                                        Edad: 
+                                        <Label.Detail >Una Edad</Label.Detail> 
+                                    </Label>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Label style={styles.rowProfileTittleStyle} color='grey'>
+                                        Fecha de Cumpleaños: 
+                                        <Label.Detail>{this.state.user.birthDate}</Label.Detail> 
+                                    </Label>                                 
+                                </Grid.Row>
+                            </Accordion.Content>
+                      
+                            <Accordion.Title active={this.state.activeInfoProfessional} onClick={this.handleInfoProfessional}>
+                                <Label style={styles.tittleProfileStyle} color='black' ribbon>Informacion Profesional</Label>
+                            </Accordion.Title>
+                            <Accordion.Content active={this.state.activeInfoProfessional}>
+                                <Grid.Row>
+                                    <Label style={styles.rowProfileTittleStyle} color='grey'>
+                                        Trabajo: 
+                                        <Label.Detail >Un puesto de trabajo</Label.Detail> 
+                                    </Label>                                 
+                                </Grid.Row><Grid.Row>
+                                    <Label style={styles.rowProfileTittleStyle} color='grey'>
+                                        Repositorio Git: 
+                                        <Label.Detail >Un Repositorio Git</Label.Detail> 
+                                    </Label>                                 
+                                </Grid.Row><Grid.Row>
+                                    <Label style={styles.rowProfileTittleStyle} color='grey'>
+                                        Perfil LinkedIn: 
+                                        <Label.Detail >Un Perfil LinkedIn</Label.Detail> 
+                                    </Label>                                 
+                                </Grid.Row><Grid.Row>
+                                    <Label style={styles.rowProfileTittleStyle} color='grey'>
+                                        Mail: 
+                                        <Label.Detail >{this.state.user.mail}</Label.Detail> 
+                                    </Label>                                 
+                                </Grid.Row>
+                            </Accordion.Content>
+                        </Accordion>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
+        )
+    }
 }
